041e15459398519d7d7a496cd0c5db9b2f45d4df
Rémi Coulombe <rem.coulombe@gmail.com>
2019-10-11 14:53:08 -0400

    TP02_loop_intensification_note01

---
+++ loop_test.py
@@ -0,0 +1,21 @@
+
+i = 0
+a = None
+def loop():
+    global a
+    a = "allo comment ca va"
+    
+
+loop()
+
+while (i != 3):
+    
+    if(i == 0):
+        print(a) 
+        
+    i+=1
+    if(i==3):
+        i = 0 
+    
+    
+
