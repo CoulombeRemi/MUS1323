c9e794e24542c0aa121bf63fe12fcd02a6df3032
Unknown <rem.coulombe@gmail.com>
2019-11-20 11:48:49 -0500

    dev03.333

--- dev03/dev03.py
+++ dev03/dev03.py
@@ -1,9 +1,12 @@
-
-
-
 from pyo import *
 import random
 s = Server().boot()
+
+# env global
+env = LinTable([(0,0), (150,1), (500,1), (5000,.5), (8191,0)], size=8192)
+globmet = Metro(time=120).play()
+globamp = TrigEnv(globmet, table=env, dur=120)
+
 
 ## ARP 01 1/64 à 90bpm
 arp_noteTime = 0.0417
@@ -17,7 +20,7 @@ arp_instru = SuperSaw(freq=arp_trigChoice , detune=0, bal=0, mul=arp_amp).mix(2)
 arp_Filter01 = ButLP(arp_instru, freq=2800)
 arp_Chorus = Chorus(arp_Filter01, depth=1, feedback=0.25, bal=0.50)
 arp_Verb = Freeverb(arp_Chorus, size=0.50, damp=0.50, bal=0.50)
-arp_Filter02 = MoogLP(arp_Verb, freq=3500, res=.22, mul=1).out()
+arp_Filter02 = MoogLP(arp_Verb, freq=3200, res=.22, mul=.9*globamp).out()
 
 ## Pad 2/1 à 90bpm
 pad_noteTime = 5.333
@@ -29,12 +32,13 @@ pad_amp = TrigEnv(pad_beat, table=pad_env, dur=pad_noteTime, mul=.25)
 
 pad_instru1 = SuperSaw(freq=pad_trigChoice , detune=0, bal=0, mul=pad_amp).mix(2)
 pad_instru2 = SumOsc(freq=pad_trigChoice, ratio=0.50, index=0.50, mul=pad_amp*1.2).mix(2)
-pad_Filter01 = MoogLP(pad_instru1, freq=2000, res=.22, mul=1).out()
-pad_Filter02 = ButBP(pad_instru2, freq=2500).out()
+pad_Filter01 = MoogLP(pad_instru1, freq=1480, res=.22, mul=.9*globamp).out()
+pad_Filter02 = ButBP(pad_instru2, freq=3000, mul=1*globamp).out()
 
 ## Bass 2/1 à 90bpm
 bass_noteTime = 5.333
-bass_notes = [midiToHz(x) for x  in [48 ,38 ,41 ,40]]
+bass_notes = [midiToHz(x) for x  in [48,38,41,40]]
+
 bass_env = ExpTable([ ( 0 , 0 ) , (100 ,1) , (500 ,.5) , (8190 ,.5) , (8191 ,0) ] , size =8192)
 bass_beat = Beat(time=bass_noteTime, taps=4, w1=100, w2=100, w3=100).play()
 bass_trigChoice = TrigChoice(bass_beat, choice=bass_notes, port=0.005)
@@ -42,7 +46,7 @@ bass_amp = TrigEnv(bass_beat, table=bass_env, dur=bass_noteTime, mul=.25)
 fil_lfo = Sine(freq=1/bass_noteTime, phase=0, mul=1, add=0)
 
 bass_instru = FM(carrier=bass_trigChoice, ratio=0.50, index=5, mul=bass_amp, add=0).mix(2)
-bass_filter = MoogLP(bass_instru, freq=fil_lfo*300, res=0, mul=.75, add=0).out()
+bass_filter = MoogLP(bass_instru, freq=fil_lfo*300, res=.2, mul=.65*globamp, add=0).out()
 
 ## Misc
 misc_noteTime = 0.0156*50 #peut-etre x5
@@ -57,7 +61,7 @@ misc_Filter1 = ButLP(misc_instru, freq=1800, mul=1)
 misc_Filter2 = ButHP(misc_Filter1, freq=800, mul=1)
 misc_delay = Delay(misc_Filter2, delay=0.35, feedback=.75, maxdelay=1, mul=.5, add=0)
 misc_chorus = Chorus([misc_Filter2,misc_delay], depth=1, feedback=0.25, bal=0.50, mul=1, add=0)
-misc_verb = Freeverb(misc_chorus, size=1, damp=0.50, bal=0.50, mul=.5, add=0).out()
+misc_verb = Freeverb(misc_chorus, size=1, damp=0.50, bal=0.50, mul=.5*globamp, add=0).out()
 
 
 
@@ -80,13 +84,16 @@ misc_verb = Freeverb(misc_chorus, size=1, damp=0.50, bal=0.50, mul=.5, add=0).ou
 
 
 
+#arp_Filter02.ctrl(title="Arp control")
+#pad_Filter01.ctrl(title="pad 1")
+#pad_Filter02.ctrl(title="pad 2")
+#bass_filter.ctrl(title="bass")
+#misc_verb.ctrl(title="misc")
 
 
 
 
 
-
-
-spectre = Spectrum(misc_Filter2, size=1024, wintype=2, function=None, wintitle="Spectrum")
+#spectre = Spectrum(misc_Filter2, size=1024, wintype=2, function=None, wintitle="Spectrum")
 
 s.gui(locals())
