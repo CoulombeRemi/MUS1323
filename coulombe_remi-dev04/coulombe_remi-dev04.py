1dfe0970c76d6aac8cd3e7fdd2273e8652832fa7
Remi <rem.coulombe@gmail.com>
2019-11-28 13:25:19 -0500

    dev04 filterQ

--- coulombe_remi-dev04/coulombe_remi-dev04.py
+++ coulombe_remi-dev04/coulombe_remi-dev04.py
@@ -1,10 +1,10 @@
 from pyo import *
 
-s = Server().boot()
+
 
 class Waha:
-    def __init__(self, frqCut, min, max, q):
-        self.filter = ButBP(sf, freq=fol*10000, q=self.q, mul=0.4, add=0)
+    def __init__(self, son, frqCut, min, max, que):
+        self.filter = ButBP(son, freq=fol*10000, q=que, mul=0.4, add=0)
         
     def out(self, chnl=0):
         self.filter.out()
@@ -12,15 +12,21 @@ class Waha:
         
     def sig(self):
         return self.filter
+        
+    print("allo")
 
+s = Server().boot()
 
 sf = SfPlayer("drum.wav", speed=1, loop=True, mul=0.4)
 fol = Follower(sf, freq=20, mul=1, add=0)
 
 
-filter.ctrl()
+
+output = Waha(sf, 200, 20, 2000, 6).out()
+output.filter.ctrl()
 
 
 
-p = Print(fol, method=0, interval=.1, message="RMS")
+
+#p = Print(fol, method=0, interval=.1, message="RMS")
 s.gui(locals())
