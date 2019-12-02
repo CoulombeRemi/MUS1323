40e44a5037bd228b8e135e9179f18ce6dc6afdee
Unknown <rem.coulombe@gmail.com>
2019-12-02 16:06:16 -0500

    dev04 - minmax

--- coulombe_remi-dev04/coulombe_remi-dev04.py
+++ coulombe_remi-dev04/coulombe_remi-dev04.py
@@ -3,9 +3,21 @@ from pyo import *
 
 
 class Waha:
-    def __init__(self, son, frqCut, min, max, que):
-        self.filter = ButBP(son, freq=fol*10000, q=que, mul=0.4, add=0)
-        
+    def __init__(self, son, min, max, que):
+        ampToFrq = amp*20000    
+        self.filter = ButBP(son, freq=ampToFrq, q=que, mul=0.4, add=0)
+
+        if(ampToFrq < min):
+            print("min")
+            self.filter.freq = min
+        if(ampToFrq > max):
+            print("max")
+            self.filter.freq = max
+
+
+        print(self.filter.freq)
+            
+
     def out(self, chnl=0):
         self.filter.out()
         return self
@@ -13,18 +25,18 @@ class Waha:
     def sig(self):
         return self.filter
         
-    print("allo")
 
 s = Server().boot()
 
 sf = SfPlayer("drum.wav", speed=1, loop=True, mul=1)
-fol = PeakAmp(sf)
+amp = PeakAmp(sf)
 
+sf.out()
 
  
-output = Waha(sf, 200, 20, 2000, 6).out()
+output = Waha(sf, 500, 2000, 6).out()
 output.filter.ctrl()
     
 
-p = Print(fol, method=0, interval=.1, message="RMS")
+#p = Print(amp, method=0, interval=.05, message="RMS")
 s.gui(locals())
