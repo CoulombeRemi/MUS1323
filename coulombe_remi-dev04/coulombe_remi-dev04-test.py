da332db0d6eda85954db1297d4dd634257aac9b2
Unknown <rem.coulombe@gmail.com>
2019-12-03 15:10:53 -0500

    dev04 min max range defined

--- coulombe_remi-dev04/coulombe_remi-dev04-test.py
+++ coulombe_remi-dev04/coulombe_remi-dev04-test.py
@@ -1,38 +1,27 @@
 from pyo import *
 
-
+# Rémi Coulombe 20130013
+# 3 décembre 2019
+# TP04 - MUS1323
 
 class Waha:
-    
-
-
-
     def __init__(self, son, min, max, que):
-        self.ampToFrq = amp*20000
-        self.min = min
-        self.max = max
-        self.filter = ButBP(son, freq=self.min_max(), q=que, mul=0.75, add=0)
-        
-        p = Print(amp, method=0, interval=0.25, message="ampToFrq: ")
+        ampToFrq = amp * (max - min) + min # define the range between the min and max
+        self.filter = ButBP(son, freq=ampToFrq, q=que, mul=0.75, add=0)
         
     # https://stackoverflow.com/questions/5996881/how-to-limit-a-number-to-be-within-a-specified-range-python
-    def min_max(self):
-        if self.ampToFrq < self.min:
-            print("mini")
-            return self.min
-        elif self.ampToFrq > self.max:
-            print("maxi")
-            return self.max
-        else:
-            print("same")
-            return self.ampToFrq
+    #def min_max(self):
+        #if self.ampToFrq < self.min:
+        #    print("mini")
+        #    return self.min
+        #elif self.ampToFrq > self.max:
+        #    print("maxi")
+        #    return self.max
+        #else:
+        #    print("same")
+        #    return self.ampToFrq
             
     # https://stackoverflow.com/questions/32447397/how-to-check-if-a-variables-value-has-changed/38143715#38143715
-
-            
-
-    #print(min_max(10, 1, 25))
-
 
     def out(self, chnl=0):
         self.filter.out()
@@ -46,16 +35,14 @@ s = Server().boot()
 
 sf = SfPlayer("drum.wav", speed=1, loop=True, mul=1)
 amp = PeakAmp(sf) ## get peak amplitude from sound file
-#sf.out()
+sf.ctrl()
+sf.out()
 
 
 
-
-
- 
-output = Waha(sf, -100, 50000, 6).out() # pass arg. to class and output it
+output = Waha(sf, 20, 20000, 6).out() # pass arg. to class and output it
 output.filter.ctrl() # access filter controls from class Waha
 
 
-# p = Print(amp, method=0, interval=0.025, message="RMS")
+
 s.gui(locals())
