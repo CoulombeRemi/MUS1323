0b66fc6001d8f1ea79bf00f556b772ca7ccaed9f
Unknown <rem.coulombe@gmail.com>
2019-12-04 21:12:11 -0500

    fin tp04

--- coulombe_remi-dev04/coulombe_remi-dev04.py
+++ coulombe_remi-dev04/coulombe_remi-dev04.py
@@ -1,24 +1,32 @@
 from pyo import *
+import os
 
-
+# Rémi Coulombe 20130013
+# 4 décembre 2019
+# TP04 - MUS1323
 
 class Waha:
-    def __init__(self, son, min, max, que):
-        ampToFrq = amp*20000    
-        self.filter = ButBP(son, freq=ampToFrq, q=que, mul=0.4, add=0)
+    def __init__(self, son, min=20, max=20000, cutFrq=30, que=3):
+        self.son = son
+        self.min = Sig(min) # minimal frq 
+        self.max = Sig(max) # maximal frq 
+        self.cutFrq = Sig(cutFrq) # cut frq
+        self.que = Sig(que) # Q
+        self.amp = Follower(self.son, freq=self.cutFrq)
+        ampToFrq = self.amp * (self.max - self.min) + self.min
+        self.filter = ButBP(self.son, freq=ampToFrq, q=self.que, mul=1)
 
-        if(ampToFrq < min):
-            print("min")
-            self.filter.freq = min
-        if(ampToFrq > max):
-            print("max")
-            self.filter.freq = max
-
-
-        print(self.filter.freq)
-            
-
-    def out(self, chnl=0):
+    ## set new freq. to sig(value)
+    def min_frq(self, min_newFq):
+        self.min.value = min_newFq
+    def max_frq(self, max_newFq):
+        self.max.value = max_newFq
+    def cut_frq(self, cut_newFq):
+        self.cutFrq.value = cut_newFq
+    def cut_frq(self, new_Q):
+        self.que.value = new_Q
+        
+    def out(self):
         self.filter.out()
         return self
         
@@ -27,16 +35,15 @@ class Waha:
         
 
 s = Server().boot()
+path = os.path.join(os.path.expanduser("~"), "Desktop\MUS1323\coulombe_remi-dev04", "wahaDrum_fullWet.wav") # settings utilisés: Waha(sf, 200, 16000, 20, 2.6)
+s.recordOptions(dur=10, filename=path, fileformat=0, sampletype=0)
 
-sf = SfPlayer("drum.wav", speed=1, loop=True, mul=1)
-amp = PeakAmp(sf)
+sf = SfPlayer("drum_dry.wav", loop=True, mul=1)#.out()
+output = Waha(sf, 200, 16000, 20, 2.6).out()
+#output.min_frq(3000)
+#output.cut_frq(2)
 
-sf.out()
 
- 
-output = Waha(sf, 500, 2000, 6).out()
 output.filter.ctrl()
-    
-
-#p = Print(amp, method=0, interval=.05, message="RMS")
+#s.recstart()
 s.gui(locals())
